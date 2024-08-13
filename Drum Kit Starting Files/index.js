197614f5ee9b2060e80635590efbc21f0eedb48c
Abhinavmanthena <164613835+Abhinavmanthena@users.noreply.github.com>
2024-08-13 13:12:54 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,53 @@
+var number = document.querySelectorAll(".drum").length;
+for (var i = 0; i < number; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var keyy = this.innerHTML;
+    makesound(keyy);
+    buttonanimation(keyy);
+  });
+}
+document.addEventListener("keypress", function (event) {
+  makesound(event.key);
+  buttonanimation(event.key);
+});
+function makesound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log("another key is pressed");
+  }
+}
+function buttonanimation(button) {
+  var activebutton = document.querySelector("." + button);
+  activebutton.classList.add("pressed");
+  setTimeout(function () {
+    activebutton.classList.remove("pressed");
+  }, 100);
+}
